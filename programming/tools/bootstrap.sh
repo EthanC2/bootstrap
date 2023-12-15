29754748675d78eab7d1c216e70abc4c2f1e8d7f
EthanC2 <ethanrcox@protonmail.com>
2023-12-15 12:06:45 -0500

    added git defaults

--- programming/tools/bootstrap.sh
+++ programming/tools/bootstrap.sh
@@ -4,3 +4,9 @@ set -$FLAGS
 
 # 1. Install development packages
 sudo apt install -y -qq $(cat programming/tools/packages.txt | tr '\n' ' ')
+
+# 2. Configure Git defaults
+git config --global user.name "EthanC2"
+git config --global user.email "ethanrcox@protonmail.com"
+git config --global core.editor "nvim"
+git config --global init.defaultBranch "main"
