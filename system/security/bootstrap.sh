f627b56735aa2b6a496cdb0b5f46217c01faef23
othrym <othrym@memento>
2023-12-15 11:45:21 -0500

    added protonvpn-cli to system/security

--- system/security/bootstrap.sh
+++ system/security/bootstrap.sh
@@ -4,3 +4,11 @@ set -$FLAGS
 
 # 1. Install security packages
 sudo apt install -y -qq $(cat system/security/packages.txt | tr '\n' ' ')
+
+# 2. Install ProtonVPN
+RELEASES_URL='https://repo.protonvpn.com/debian/dists/stable/main/binary-all/'
+PACKAGE=$(curl -s $RELEASES_URL | grep -o "\"protonvpn-stable-release.*\.deb\"" | tr -d '"' | sort -V | head -n 1)
+wget -q $RELEASES_URL/$PACKAGE
+sudo dpkg -i $PACKAGE
+sudo apt update -y
+sudo apt install protonvpn-cli
